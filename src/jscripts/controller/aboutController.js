8a4998aafbe70dd503b054a4ef7de094b6d330b1
swsun <tomsunfive@gmail.com>
2016-04-13 13:50:22 +0800

    remove the FCC info in the about page

--- src/jscripts/controller/aboutController.js
+++ src/jscripts/controller/aboutController.js
@@ -2,27 +2,20 @@ app.controller('aboutController', function ($scope, $location, Ajax) {
     
 	$scope.aboutData = {};
 
-	function generateAboutData(data) {
-		var aboutData = [];
-
-		for (var i = 0; i < data.data.length; i++) {
-	        var text = data.data[i].Text;
-	        var name = data.data[i]['_name'];
-	        if (name.indexOf(':') != name.length - 1) {
-	            name += ':';
-	        }
-	        aboutData.push({label: name, text: text});
-	    }
-
-	    return aboutData;
-	}
     // get about content
     $scope.getAbout = function () {
         var cmd = "opcode=show&ip=127.0.0.1&cmd='show about'";
 
         Ajax.doRequest(cmd, function (data) {
-        	$scope.aboutData = generateAboutData(data);
-        }, true);
+        	var simpleData = {};
+        	// remove the FCC info
+        	angular.forEach(data, function(value, key) {
+        		if (key.indexOf('FCC') == -1) {
+        			this[key] = value;
+        		}
+			}, simpleData);
+        	$scope.aboutData = simpleData;
+        }, true, {'lowerCase': false, 'removeKeySpace': false});
     };
 
     $scope.getAbout();
