8b31b5ef472660639e34515847446755489edfd3
swsun <tomsunfive@gmail.com>
2016-06-08 14:14:31 +0800

    add Table component

---
+++ src/jscripts/ui/widget/table.jsx
@@ -0,0 +1,183 @@
+
+import React, {Component} from 'react';
+
+class Table extends Component {
+	constructor (props) {
+	    super(props);
+
+	    this.state = {
+	    	data: props.dataSource,
+	    	sort: {},
+	    	selectedRow: null
+	    };
+	}
+
+	componentDidMount () {
+	    console.log('componentDidMount');
+	}
+
+  	componentWillReceiveProps (nextProps) {
+  		console.log('componentWillReceiveProps');
+  	}
+
+  	componentDidUpdate () {
+    	console.log('componentDidUpdate');
+  	}
+
+  	createTitle () {
+  		const title = this.props.title;
+  		let titleContent = title;
+
+  		if (title && title.render) {
+			titleContent = title.render.call(this, title.text || '');
+  		} 
+
+  		return titleContent ? <div className="title_heading">{titleContent}</div> : '';
+  	}
+
+  	getSortClass (asc) {
+  		const sortClassMapping = {0: 'down', 1: 'up'};
+
+  		return sortClassMapping[asc];
+  	}
+
+  	onSort (col) {
+  		const key = col.dataIndex;
+  		const asc = this.state.sort.asc == undefined ? 1 : (this.state.sort.asc == 1 ? 0 : 1);
+
+  		this.state.sort = {key: key, asc: asc};
+
+  		this.sortData(key, asc);
+  	}
+
+  	onRowSelect (record) {
+  		let rowKey = this.props.rowKey;
+  		let selectedRow = this.state.selectedRow;
+  		if (selectedRow && (selectedRow == record[rowKey])) {
+  			selectedRow = null;
+  		} else {
+  			selectedRow = record[rowKey];
+  		}
+  		
+  		this.setState({selectedRow});
+  	}
+
+  	getSelectedRow () {
+  		return this.state.selectedRow;
+  	}
+
+  	getColumnKey (column, index) {
+    	return column.key || column.dataIndex || index;
+  	}
+
+	createHeader () {
+		const sortable = this.props.sortable ? 'sortable' : '';
+		const sortObj = this.state.sort;
+		let headers = [];
+
+		this.props.columns.map((h, i) => {
+			if (sortable) {
+				//class="sort down"
+				let sortClass = '';
+				let clickFn = this.onSort.bind(this, h);
+
+				if (sortObj && sortObj.key) {
+					if (h.dataIndex == sortObj.key) {
+						sortClass += 'sort ' + this.getSortClass(sortObj.asc);
+					}
+				}
+
+				headers.push(<th className={sortClass} onClick={clickFn}>{h.name}</th>);
+		    } else {
+		    	headers.push(<th>{h.name}</th>);
+		    }
+			
+		});
+
+		return <thead className={sortable}><tr>{headers}</tr></thead>;
+	}
+
+	createCell (row, col, index) {
+		const colKey = this.getColumnKey(col);
+		let content = row[colKey] || '';
+		
+		if (col.render) {
+			content = col.render.call(this, content, row, index);
+		}
+
+		return <td key={colKey}>{content}</td>;
+	}
+
+	createRow (row, cols, index) {
+		let rowKey = this.props.rowKey;
+		let onClickFn = this.onRowSelect.bind(this, row);
+		let rowClass = this.state.selectedRow == row[rowKey] ? 'selected' : '';
+
+		return <tr key={row[rowKey]} className={rowClass} onClick={onClickFn}>{cols}</tr>;
+	}
+
+	createBody (data) {
+		data = this.state.data;
+
+		let rows = data.map((d, i) => {
+			let tds = [];
+			this.props.columns.map((h, j) => {
+				tds.push(this.createCell(d, h, j));
+			});
+
+			return this.createRow(d, tds, i);
+
+		});
+		
+		return <tbody>{rows}</tbody>;
+	}
+
+	createFooter () {
+		// TBD
+	}
+
+	sortData (key, asc) {
+	    let data = this.state.data;
+	    data = data.sort(function(a, b) {
+	      	var x = a[key];
+	      	var y = b[key];
+	      	if (asc) {
+	        	return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+	      	} else {
+	        	return ((x > y) ? -1 : ((x < y) ? 1 : 0));
+	      	}
+	    });
+
+	    this.setState({ data });
+	}
+
+	render () {
+		const table = (
+			<div className="table_wrapper">
+				{this.createTitle()}
+				<table className="data responsive">
+					{this.createHeader()}
+					{this.createBody()}
+					{this.createFooter()}
+				</table>
+			</div>
+		);
+
+		return (
+			<div className="panel_wrapper">
+				<div className="panel table rounded white no_pad">
+	          		{table}
+          		</div>
+			</div>
+		)
+	}
+} 
+
+Table.defaultProps = {
+	columns: [],
+	dataSource: [],
+	sortable: false,
+	rowKey: 'name'
+};
+
+module.exports = Table;
